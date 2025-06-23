1515eebc3a5a9e360fcfeb101bb97fac5b7e0831
mohamed <sickmido@gmail.com>
2025-06-23 14:29:02 +0300

    Added initial setup

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from "express";
+const app = express();
+
+const ORIGIN = process.env.ORGIN || "localhost:3000";
+import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+  message: {
+    status: 429,
+    message: "Too many requests, please try again later.",
+  },
+});
+
+app.use(cors({ origin: ORIGIN }));
+app.use(helmet());
+app.use(limiter);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    sucess: true,
+    message: "Hello World!",
+    data: null,
+  });
+});
+
+export default app;
